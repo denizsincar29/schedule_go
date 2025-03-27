77c3188972e7dbfad6e94dd7f7eb3a8853f5668e
deniz <deniz@r1oaz.ru>
2025-03-27 15:14:53 +0300

    Fixed null datetime parsing, added various date format parsing from json.

--- modeus/datetype.go
+++ modeus/datetype.go
@@ -1,42 +1,68 @@
 package modeus
 
 import (
+	"encoding/json"
+	"strings"
 	"time"
 )
 
 type DateTime time.Time
 
-// marshal unmarshal
-func (dt *DateTime) MarshalJSON() ([]byte, error) {
-	return time.Time(*dt).MarshalJSON()
+const (
+	isoFullDateTimeTZFormat    = time.RFC3339
+	isoFullDateTimeLocalFormat = "2006-01-02T15:04:05" // ISO 8601 without timezone
+	isoDateFormat              = "2006-01-02"
+)
+
+// MarshalJSON implements the Marshaler interface for DateTime.
+func (dt DateTime) MarshalJSON() ([]byte, error) {
+	if dt.IsNull() {
+		return json.Marshal(nil)
+	}
+	t := time.Time(dt)
+	return json.Marshal(t.Format(isoFullDateTimeTZFormat))
 }
 
+// UnmarshalJSON implements the Unmarshaler interface for DateTime.
 func (dt *DateTime) UnmarshalJSON(data []byte) error {
-	// delete surrounding quotes if any
-	if len(data) > 2 && data[0] == '"' && data[len(data)-1] == '"' {
-		data = data[1 : len(data)-1]
+	str := string(data)
+	// if null, set to zero value
+	if str == "null" {
+		*dt = DateTime(time.Time{})
+		return nil
 	}
-	// try to unmarshal as time.Time first
+	// Remove quotes if they exist
+	str = strings.Trim(str, "\"")
+
 	var t time.Time
-	err := t.UnmarshalJSON(data)
+	var err error
+
+	// 1. Try parsing as full ISO 8601 with timezone
+	t, err = time.Parse(isoFullDateTimeTZFormat, str)
 	if err == nil {
 		*dt = DateTime(t)
 		return nil
 	}
-	// now try to parse without timezone
-	t, err = time.Parse("2006-01-02T15:04:05", string(data))
+
+	// 2. Try parsing as full ISO 8601 without timezone (using local timezone)
+	t, err = time.Parse(isoFullDateTimeLocalFormat, str)
 	if err == nil {
 		*dt = DateTime(t)
 		return nil
 	}
-	// now try to parse without time completely
-	t, err = time.Parse("2006-01-02", string(data))
+
+	// 3. Try parsing as ISO 8601 date
+	t, err = time.Parse(isoDateFormat, str)
 	if err == nil {
 		*dt = DateTime(t)
 		return nil
 	}
-	// don't want any errors here, return zero time
-	//log.Printf("DateTime.UnmarshalJSON: %v", err)
-	*dt = DateTime(time.Time{})
-	return nil
+
+	// If all parsing attempts fail, return the last error encountered
+	return err
+}
+
+// IsNull returns true if the DateTime is the zero value.
+func (dt DateTime) IsNull() bool {
+	return time.Time(dt).IsZero()
 }
